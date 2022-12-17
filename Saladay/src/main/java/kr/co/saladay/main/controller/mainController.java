1dff941528897eb0fb08adbbea326422e58db105
2-Sungho <sungho._.2@icloud.com>
2022-12-17 13:44:13 +0900

    12/17 main.jsp 연결

---
+++ Saladay/src/main/java/kr/co/saladay/main/controller/mainController.java
@@ -0,0 +1,16 @@
+package kr.co.saladay.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+
+	/** 메인 페이지
+	 * @return main.jsp 포워드
+	 */
+	@GetMapping("/")
+	public String main() {
+		return "main";
+	}
+}
