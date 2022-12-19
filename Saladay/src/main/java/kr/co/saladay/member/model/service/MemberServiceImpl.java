31b69e6d2f06db77eae0ad49fef537b2f73d7d04
use-da <110652066+use-da@users.noreply.github.com>
2022-12-19 12:33:46 +0900

    회원가입 기능

--- Saladay/src/main/java/kr/co/saladay/member/model/service/MemberServiceImpl.java
+++ Saladay/src/main/java/kr/co/saladay/member/model/service/MemberServiceImpl.java
@@ -1,15 +1,34 @@
 package kr.co.saladay.member.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.co.saladay.member.model.dao.MemberDAO;
+import kr.co.saladay.member.model.vo.Member;
 
 @Service
 public class MemberServiceImpl implements MemberService{
 	
 	@Autowired
 	private MemberDAO dao;
+	
+	@Autowired
+	BCryptPasswordEncoder bcrypt;
+
+	
+	
+	//회원가입
+	@Transactional
+	@Override
+	public int signUp(Member member) {
+		
+		// 비밀번호 암호화
+		member.setMemberPw(bcrypt.encode(member.getMemberPw()));
+		
+		return dao.signUp(member);
+	}
 	
 	
 	
