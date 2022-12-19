c78e755ff1adec0350a7f8a6759d92b387ff2d8a
jihoon-git <hooon.h@gmail.com>
2022-12-19 16:27:41 +0900

    mybatis-config에 review-mapper 추가

--- Saladay/src/main/java/kr/co/saladay/review/model/vo/Review.java
+++ Saladay/src/main/java/kr/co/saladay/review/model/vo/Review.java
@@ -1,5 +1,7 @@
 package kr.co.saladay.review.model.vo;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,5 +10,16 @@ import lombok.ToString;
 @Setter
 @ToString
 public class Review {
+	private int reviewNo; // 리뷰번호 REVIEW_NO
+	private int rating; // 별점 RATING
+	private String reviewContent; // 리뷰 내용 REVIEW_CONTENT
+	private String reviewDate; // 리뷰 작성일 REVIEW_DATE
+	private String reviewDeleteFlag; //	리뷰 삭제여부 REVIEW_DEL_FL
+	private int memberNo; //	리뷰 작성자 번호 MEMBER_NO
+	private String memberNickname;//  리뷰 작성자 닉네임 MEMBER_NICKNAME
+	private int orderMenuNo; //	주문한 메뉴 번호 ORDER_MENU_NO
+	
+    // 이미지 목록
+    private List<ReviewImage> imageList;
 
 }
--- Saladay/src/main/java/kr/co/saladay/review/model/vo/ReviewImage.java
+++ Saladay/src/main/java/kr/co/saladay/review/model/vo/ReviewImage.java
@@ -8,5 +8,12 @@ import lombok.ToString;
 @Setter
 @ToString
 public class ReviewImage {
+	
+	private int reviewImageNo; //	리뷰 이미지 번호 REVIEW_IMG_NO
+	private String imagePath; //	이미지 경로 IMG_PATH
+	private String imageRename; //	이미지 변경명 IMG_RENAME
+	private String imageOriginal; //	이미지 원본명 IMG_ORIGINAL
+	private int imageOrder; //	이미지 순서 IMG_ORDER
+	private int reviewNo; //	리뷰 번호 REVIEW_NO
 
 }
