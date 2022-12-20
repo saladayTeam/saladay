f31994aae1fc53d30f0637415207e894a1b58d23
jihoon-git <hooon.h@gmail.com>
2022-12-20 18:38:54 +0900

    12/20 리뷰 목록 조회 완료(정렬/검색 미완)

--- Saladay/src/main/java/kr/co/saladay/review/controller/ReviewController.java
+++ Saladay/src/main/java/kr/co/saladay/review/controller/ReviewController.java
@@ -1,7 +1,13 @@
 package kr.co.saladay.review.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.co.saladay.review.model.service.ReviewService;
 
@@ -10,5 +16,16 @@ public class ReviewController {
 	
 	@Autowired
 	private ReviewService service;
+	
+	//리뷰 목록 조회
+	@GetMapping("/review")
+	public String selectReviewList(Model model,
+			@RequestParam(value="cp", required=false, defaultValue = "1") int cp){
+		
+		Map<String, Object> map = service.selectReviewList(cp);
+		model.addAttribute("map", map);
+		
+		return "/review/reviewList";
+	}
 
 }
--- Saladay/src/main/java/kr/co/saladay/review/model/dao/ReviewDAO.java
+++ Saladay/src/main/java/kr/co/saladay/review/model/dao/ReviewDAO.java
@@ -1,13 +1,38 @@
 package kr.co.saladay.review.model.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import kr.co.saladay.review.model.vo.Pagination;
+import kr.co.saladay.review.model.vo.Review;
 
 @Repository
 public class ReviewDAO {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	/** 전체 리뷰 수 조회
+	 * @return
+	 */
+	public int getListCount() {
+		return sqlSession.selectOne("reviewMapper.getListCount");
+	}
+
+	/** 전체 리뷰 목록 조회
+	 * @param pagination
+	 * @return
+	 */
+	public List<Review> selectReviewList(Pagination pagination) {
+		int offset = (pagination.getCurrentPage()-1)*pagination.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		
+		return sqlSession.selectList("reviewMapper.selectReviewList", null, rowBounds);
+	}
 
 }
