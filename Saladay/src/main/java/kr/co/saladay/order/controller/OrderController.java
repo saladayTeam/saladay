e0a29dc75309af0275d6af065efcb10b4c298a0f
2-Sungho <sungho._.2@icloud.com>
2022-12-25 22:02:29 +0900

    12/25 주문완료페이지 jsp 연결

--- Saladay/src/main/java/kr/co/saladay/order/controller/OrderController.java
+++ Saladay/src/main/java/kr/co/saladay/order/controller/OrderController.java
@@ -3,10 +3,13 @@ package kr.co.saladay.order.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import kr.co.saladay.cart.model.vo.Cart;
 import kr.co.saladay.member.model.vo.Member;
 import kr.co.saladay.order.model.service.OrderService;
+import kr.co.saladay.order.model.vo.Order;
 
 @Controller
 public class OrderController {
@@ -16,8 +19,21 @@ public class OrderController {
 
 	// 주문/결제하기 페이지
 	@GetMapping("/order")
-	public String orderPage() {
+	public String order() {
 
 		return "order/order";
 	}
+	
+	@PostMapping("/orderView")
+	public String orderView(Order order,
+			@SessionAttribute("loginMember") Member loginMember,
+			@SessionAttribute("cart") Cart cart) {
+		
+		order.setCart(cart);
+		
+		
+		
+		return "order/orderView";
+	}
+	
 }
