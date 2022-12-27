678ba315c57e04e31d8da4e2d78a995b3eaa0137
zon-d <zon_d@icloud.com>
2022-12-27 17:55:50 +0900

    2022.12.27 이름 수정

---
+++ Saladay/src/main/java/kr/co/saladay/admin/model/dao/DeliveryDAO.java
@@ -0,0 +1,25 @@
+package kr.co.saladay.admin.model.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.saladay.admin.model.vo.Delivery;
+
+@Repository
+public class DeliveryDAO {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	// 배송현황 조회
+	public List<Delivery> adminDelivery() {
+		
+		return sqlSession.selectList("");
+	}
+	
+	
+
+}
--- Saladay/src/main/java/kr/co/saladay/admin/model/service/CallendarService.java
+++
@@ -1,5 +0,0 @@
-package kr.co.saladay.admin.model.service;
-
-public class CallendarService {
-
-}
