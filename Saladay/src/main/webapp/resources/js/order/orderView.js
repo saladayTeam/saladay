2f14bc05b383f08bddfc2f4ef8d08e416b173ed1
2-Sungho <sungho._.2@icloud.com>
2022-12-28 12:34:22 +0900

    12/28 결제오류 수정

--- Saladay/src/main/webapp/resources/js/order/orderView.js
+++ Saladay/src/main/webapp/resources/js/order/orderView.js
@@ -3,5 +3,8 @@ const continueShopping=document.getElementById("continueShopping");
 
 myOrderView.addEventListener("click",()=>{
     location.href="/member/myPage/selectMyOrder";
+})
+
+continueShopping.addEventListener("click",()=>{
     location.href="/menu/packageList";
 })
