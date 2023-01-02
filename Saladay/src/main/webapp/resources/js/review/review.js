b56ffcda898426470f07e834c85ec1bd13e76032
jihoon-git <hooon.h@gmail.com>
2023-01-02 11:41:16 +0900

    리뷰 좋아요 다중 실행 오류 해결

--- Saladay/src/main/webapp/resources/js/review/review.js
+++ Saladay/src/main/webapp/resources/js/review/review.js
@@ -17,7 +17,7 @@ const modalClose=document.getElementById("modal-close");
         // Modal 닫기 버튼
         modalClose.addEventListener("click", () =>{
             modal.style.display="none";
-            location.reload("");
+            /* location.reload(""); */
         });
     }
 })();
@@ -237,56 +237,7 @@ function selectReviewDetail(reviewNo, reviewMemberNo){
                 reviewHeart.classList.add("fa-solid");
             }
 
-            /* 좋아요 증가/감소 ajax 실행 */
-            if (reviewHeart != null) {
-
-                reviewHeart.addEventListener("click", e =>{
-                    // 로그인 상태가 아닌 경우
-                    if(memberNo==""){
-                        alert("로그인 후 이용해주세요.")
-                        return;
-                    }
-            
-                    // 로그인 상태이면서 좋아요 상태가 아닌 경우
-                    if(reviewHeart.classList.contains('fa-regular')){ //빈하트인 경우 좋아요 증가
-            
-                        $.ajax({
-                            url : "/review/likeUp",
-                            data : {"reviewNo" : reviewNo , "memberNo" : memberNo},
-                            type : "GET" ,
-                            success : (result)=>{
-                                if(result >0){ // 성공
-                                    reviewHeart.setAttribute("src", "/resources/images/review/heart02.png");
-                                    reviewHeart.classList.remove('fa-regular'); // 빈하트 클래스 삭제
-                                    reviewHeart.classList.add('fa-solid'); // 채워진 하트 클래스 추가
-                                    reviewLike.innerText = Number(reviewLike.innerText)+1; // 1증가
-                                }else{ // 실패
-                                    console.log("증가 실패");
-                                }
-                            },
-                            error : ()=>{ console.log("증가 에러"); }
-                        });
-            
-                    } else{ // 채워진 하트인 경우 좋아요 감소
-                        $.ajax({
-                            url : "/review/likeDown",
-                            data : {"reviewNo" : reviewNo , "memberNo" : memberNo},
-                            type : "GET" ,
-                            success : (result)=>{
-                                if(result >0){ // 성공
-                                    reviewHeart.setAttribute("src", "/resources/images/review/heart01.png");
-                                    reviewHeart.classList.add('fa-regular'); // 빈하트 클래스 추가
-                                    reviewHeart.classList.remove('fa-solid'); // 채워진 하트 클래스 삭제
-                                    reviewLike.innerText = Number(reviewLike.innerText)-1; // 1감소
-                                }else{ // 실패
-                                    console.log("감소 실패");
-                                }
-                            },
-                            error : ()=>{ console.log("감소 에러"); }
-                        });   
-                    }
-                });
-            }
+            reviewHeart.setAttribute("reviewNo", rDetail[0].reviewNo);
         },
         error : function(req, status, error){
             console.log("에러 발생");
@@ -320,3 +271,57 @@ function selectReviewDetail(reviewNo, reviewMemberNo){
 //         });
 //     }
 // }
+
+/* 좋아요 증가/감소 ajax 실행 */
+if (reviewHeart != null) {
+
+    /* reviewHeart.removeEventListener("click", null); */
+    reviewHeart.addEventListener("click", e =>{
+        // 로그인 상태가 아닌 경우
+        if(memberNo==""){
+            alert("로그인 후 이용해주세요.")
+            return;
+        }
+
+        // 로그인 상태이면서 좋아요 상태가 아닌 경우
+        if(reviewHeart.classList.contains('fa-regular')){ //빈하트인 경우 좋아요 증가
+
+            $.ajax({
+                url : "/review/likeUp",
+                data : {"reviewNo" : reviewHeart.getAttribute("reviewNo")
+                 , "memberNo" : memberNo},
+                type : "GET" ,
+                success : (result)=>{
+                    if(result >0){ // 성공
+                        reviewHeart.setAttribute("src", "/resources/images/review/heart02.png");
+                        reviewHeart.classList.remove('fa-regular'); // 빈하트 클래스 삭제
+                        reviewHeart.classList.add('fa-solid'); // 채워진 하트 클래스 추가
+                        reviewLike.innerText = Number(reviewLike.innerText)+1; // 1증가
+                    }else{ // 실패
+                        console.log("증가 실패");
+                    }
+                },
+                error : ()=>{ console.log("증가 에러"); }
+            });
+
+        } else{ // 채워진 하트인 경우 좋아요 감소
+            $.ajax({
+                url : "/review/likeDown",
+                data : {"reviewNo" : reviewHeart.getAttribute("reviewNo")
+                , "memberNo" : memberNo},
+                type : "GET" ,
+                success : (result)=>{
+                    if(result >0){ // 성공
+                        reviewHeart.setAttribute("src", "/resources/images/review/heart01.png");
+                        reviewHeart.classList.add('fa-regular'); // 빈하트 클래스 추가
+                        reviewHeart.classList.remove('fa-solid'); // 채워진 하트 클래스 삭제
+                        reviewLike.innerText = Number(reviewLike.innerText)-1; // 1감소
+                    }else{ // 실패
+                        console.log("감소 실패");
+                    }
+                },
+                error : ()=>{ console.log("감소 에러"); }
+            });   
+        }
+    });
+}
