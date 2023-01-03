82ffcf606764fe2683bd8321fb67ce0991221425
Sseohee <108666056+SseoheeKim@users.noreply.github.com>
2023-01-03 12:32:48 +0900

    1/3 메뉴/옵션 관리 이미지 모달창 띄우기

--- Saladay/src/main/webapp/resources/js/admin/menuManage/updateMenu.js
+++ Saladay/src/main/webapp/resources/js/admin/menuManage/updateMenu.js
@@ -60,3 +60,40 @@ if(addBtn!= null) {
     })
 }
 
+
+// 메뉴명 클릭시 modal 출력 -> 즉시 실행 함수 사용 ()()
+(()=>{
+    const imgList = document.getElementsByClassName("m-img");
+
+    if(imgList.length > 0) { 
+
+        // Modal 관련 요소 얻어오기
+        const modal = document.querySelector(".modal");
+        const modalClose = document.getElementById("modal-close");
+        const modalImage = document.getElementById("modal-image");
+
+        // 클릭 이벤트 추가
+        for(let i of imgList) {
+            i.addEventListener("click", () => {
+
+                // .show 없으면 추가 (있으면 삭제) => toggle 사용
+                modal.classList.toggle("show"); 
+
+                // 클릭한 이미지 src속성값을 얻어와서 modal-image의 src속성으로 세팅
+                modalImage.setAttribute("src", i.getAttribute("src"));
+
+            });
+        }
+
+
+        // x버튼 동작
+        modalClose.addEventListener("click", () => {
+
+            // 0.5초 후에 show, hide 클래스를 모두 제거
+            setTimeout(()=>{
+                modal.classList.remove("show", "hide");
+            }, 500);
+        });
+        
+    }
+})();
