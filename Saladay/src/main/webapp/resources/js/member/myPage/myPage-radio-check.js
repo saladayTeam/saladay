4b4a8dd0d92da92f63e4983183324077ebec61b7
use-da <110652066+use-da@users.noreply.github.com>
2023-01-05 10:29:19 +0900

    페이징 URL radio-check

--- Saladay/src/main/webapp/resources/js/member/myPage/myPage-radio-check.js
+++ Saladay/src/main/webapp/resources/js/member/myPage/myPage-radio-check.js
@@ -3,8 +3,9 @@
 
 //URL주소가 맨 뒤가 ""일 때 radio 버튼 체크
 let URL = window.location.href;
-let splitURL = URL.split("/");
-let lastURL = splitURL[splitURL.length - 1];
+let splitURL = URL.split("?");
+let split2URL = splitURL[0].split("/");
+let lastURL = split2URL[split2URL.length - 1];
 
 if(lastURL == "info") {
     document.getElementById("myPageType1").checked = true;
