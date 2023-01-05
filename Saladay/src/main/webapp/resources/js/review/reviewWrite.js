f05db48db51ceaa08d97913070c98d1115ff9d6b
jihoon-git <hooon.h@gmail.com>
2023-01-06 01:02:54 +0900

    리뷰 작성 관련 js 주석 추가

--- Saladay/src/main/webapp/resources/js/review/reviewWrite.js
+++ Saladay/src/main/webapp/resources/js/review/reviewWrite.js
@@ -5,16 +5,15 @@ const drawStar = (target) => {
 }
 
 /* 리뷰 이미지 */
-const inputImage = document.getElementsByClassName("inputImage");
-const preview = document.getElementsByClassName("preview");
-const deleteImage = document.getElementsByClassName("delete-image");
+const inputImage = document.getElementsByClassName("inputImage"); // 리뷰이미지 
+const preview = document.getElementsByClassName("preview"); // 미리보기
 
 /* 이미지 삭제 관련 */
-const deleteList = document.getElementById("deleteList");
-const deleteSet = new Set();
+const deleteImage = document.getElementsByClassName("delete-image"); // 이미지 삭제버튼
 
-
+/* 이미지 id에 붙일 번호(하나씩 추가될때마다 +1) */
 let idCount = 1;
+/* 리뷰이미지 영역이 몇개인지 체크하는 변수 */
 let reviewImgCount = 0;
 
 
@@ -23,48 +22,64 @@ for(let i=0; i<inputImage.length; i++){
 
     inputImage[i].addEventListener("change", function(){
 
-        if(this.files[0] != undefined){
-            const reader = new FileReader();
-            reader.readAsDataURL(this.files[0]);
+        if(this.files[0] != undefined){ // 이미지 파일이 undefined가 아닐 때
+            const reader = new FileReader(); // FileReader() : 자바스크립트 파일입출력 객체, 비동기로 작동
+            reader.readAsDataURL(this.files[0]); // readAsDataURL : 파일 객체를 읽은 후 데이터 URL 표현으로 변환
 
             reader.onload = function(e){ 
+                // FileReader.onload() 읽기 동작이 성공적으로 완료되었을 때마다 발생
+                // 비동기이므로 원하는 동작을 위해 CALLBACK 함수를 이용
+
+                // 프리뷰 영역에 파일리더로 읽어온 result url을 setAttribute
                 preview[i].setAttribute("src", e.target.result);
                 
+                // 리뷰이미지영역 갯수 +1
                 reviewImgCount++;
 
+                // 기존에 있던 이미지의 아이디 지우고
+                // 이미지 아이디는 이미지 순서별로 다르니까 지워줘야함
                 inputImage[i].removeAttribute("id");
 
+                // 리뷰이미지 div 생성 함수
                 addReviewImg();
             }
-        } else {
+        } else { // 이미지 파일이 undefined 일때 프리뷰의 result url remove
             preview[i].removeAttribute("src");
         }
 
     });
 
-    // 미리 보기 삭제 버튼
+    // 미리 보기 삭제 버튼 눌렀을 때
     deleteImage[i].addEventListener("click", function(){
     
-        // 미리보기가 존재하는 경우에만
+        // 미리보기 이미지 안에 소스(src)가 존재하는 경우에만
         if( preview[i].getAttribute("src") != null){
             
+            // 기존에 존재하던 미리보기 이미지 소스(src) 삭제
             preview[i].removeAttribute("src");
     
+            // 입력한 이미지 값 삭제
             inputImage[i].value = "";
             
-            deleteSet.add[i];
+            // 이미지 삭제 리스트에 추가
+/*             deleteSet.add[i]; */
 
+            // 리뷰이미지 클래스(프리뷰+인풋이미지+삭제버튼을 포함한 div)가 2개이상 있을 때
             if(document.getElementsByClassName("reviewImg").length > 1){
-                this.parentElement.remove();
+                this.parentElement.remove(); // 리뷰이미지 div 삭제
 
-                reviewImgCount--;
+                reviewImgCount--; // 리뷰 이미지 영역 -1
 
-                if(reviewImgCount == 2){
+                if(reviewImgCount == 2){ // 리뷰이미지 영역이 2개일때 삭제 버튼을 눌렀을 경우
                     addReviewImg();
+                    // 리뷰이미지 미리보기 area 생성
+                    // 남은 하나를 지워도 추가 생성된 div에서 이미지를 다시 추가할 수 있기 때문
                 }
-            }else{
-                preview[i].removeAttribute("src");
-                this.previousElementSibling.value = "";
+
+            }else{ //리뷰이미지 클래스(프리뷰+인풋이미지+삭제버튼을 포함한 div)가 1개이거나 없을때
+                preview[i].removeAttribute("src"); // 미리보기 이미지 소스(src) 삭제
+                this.previousElementSibling.value = ""; // 인풋이미지 값 삭제
+                // 2개일때 만들어둔 div에서 다시 이미지 추가 가능
             }
         }
     
@@ -72,22 +87,28 @@ for(let i=0; i<inputImage.length; i++){
 }
 
 
-
+// 리뷰이미지 조립 함수
 function addReviewImg(){
 
+    // 리뷰이미지 클래스(프리뷰+인풋이미지+삭제버튼을 포함한 div)가 3개 이하일 때만 생성한다
+    // 이미지는 3개까지만 입력 가능하므로
     if(document.getElementsByClassName("reviewImg").length < 3){
+
+        // reviewImg
         const div = document.createElement("div");
         div.classList.add("reviewImg");
         
         const label = document.createElement("label");
         label.setAttribute("for" , "img" + idCount);
+        // idCount 이미지 id에 붙이는 번호(하나씩 추가될때마다 +1)
         
+        // 미리보기(preview)
         const img = document.createElement("img");
         img.classList.add("preview");
     
         label.append(img);
     
-    
+        // inputImage
         const input = document.createElement("input");
         input.classList.add("inputImage");
         input.setAttribute("type", "file");
@@ -96,15 +117,18 @@ function addReviewImg(){
         input.setAttribute("id", "img" + idCount);
     
     
+        // delete-image
         const span = document.createElement("span");
         span.classList.add("delete-image");
         span.innerHTML = "&times;";
     
         div.append(label, input, span);
-        idCount++;
+        idCount++; // div 조립 하나씩 끝날때마다 idCount++
     
-        document.querySelector(".rw-image-area").append(div);
+        document.querySelector(".rw-image-area").append(div); // 조립 끝난 reviewImg div를 부모 div에 append
     
+        // 인풋이미지 함수, 위에 작성한 함수와 같음.
+        // 새로 조립할때마다 같이 만들어줘야 비동기로 생성된 div에서도 동일하게 함수가 작동.
         input.addEventListener("change", function(){
     
             if(this.files[0] != undefined){
@@ -125,9 +149,10 @@ function addReviewImg(){
     
         });
 
+        // 미리보기 삭제버튼 함수. 마찬가지로 위에 작성한 함수와 같음.
         span.addEventListener("click", function(){
             const preview = this.previousElementSibling.previousElementSibling.children[0];
-            //미리보기가 존재하는 경우에만
+
             if( preview.getAttribute("src") != null){
                 
                 preview.removeAttribute("src");
