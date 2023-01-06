47353989c4096ad2715cf9240a3c5d62debfbf00
Sseohee <108666056+SseoheeKim@users.noreply.github.com>
2023-01-06 10:11:20 +0900

    주석수정

--- Saladay/src/main/webapp/resources/js/admin/menuManage/updateMenu.js
+++ Saladay/src/main/webapp/resources/js/admin/menuManage/updateMenu.js
@@ -2,22 +2,25 @@ console.log("메뉴관리");
 
 // 체크박스
 $(document).ready(function() {
+    // 전체선택 시 개별 체크 전부 선택
     $("#allCheck").click(function() {
 		if($("#allCheck").is(":checked")) $("input[name=rowCheck]").prop("checked", true);
 		else $("input[name=rowCheck]").prop("checked", false);
 	});
     
+    // 개별 선택시 
     $("input[name=rowCheck]").click(function() {
         var total = $("input[name=rowCheck]").length;
         var checked = $("input[name=rowCheck]:checked").length;
 
+        // 전체선택해제
         if(total != checked) $("#allCheck").prop("checked", false);
         else $("#allCheck").prop("checked", true); 
     }); 
 });
 
 
-// 메뉴 삭제
+// 메뉴 삭제(삭제버튼에 onclick이벤트)
 function deleteValue(){
     const valueArr = new Array();
     const list =  document.getElementsByName("rowCheck");
@@ -57,13 +60,23 @@ function deleteValue(){
 // 메뉴 추가 팝업창
 const addBtn = document.getElementById("addBtn");
 
-if(addBtn!= null) {
+if(addBtn!= null) { 
     addBtn.addEventListener("click", function(){
         
+        // 메뉴 추가 버튼 클릭 시 팝업창(window.open())
         const options = "width=610, height=600, left=-200, top=-900";
         window.open("/admin/regist/menu", "popupWindow", options)
     })
 }
+
+
+// 메뉴 수정
+function updateValue(menuNo) {
+    const options = "width=610, height=600, left=-200, top=-900";
+    window.open("/admin/menu/update?menuNo="+menuNo, "popupWindow", options)
+
+}
+
 
 
 // 메뉴명 클릭시 메뉴 이미지 modal 출력 -> 즉시 실행 함수 사용 ()()
@@ -104,10 +117,3 @@ if(addBtn!= null) {
 // })();
 
 
-
-// 메뉴 수정
-function updateValue(menuNo) {
-    const options = "width=610, height=600, left=-200, top=-900";
-    window.open("/admin/menu/update?menuNo="+menuNo, "popupWindow", options)
-
-}
